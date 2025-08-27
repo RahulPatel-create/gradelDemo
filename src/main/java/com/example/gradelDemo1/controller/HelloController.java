ebd03273f203150eb5439870c911421e117ab9f0
“RahulPatel-create” <rahulmicro1995@outlook.com>
2025-08-27 17:08:05 +0530

    Initial commit of local project

---
+++ src/main/java/com/example/gradelDemo1/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.gradelDemo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	public HelloController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@GetMapping("/hello")
+	public String CreateUser() {
+		return "user is created";
+	}
+
+}
